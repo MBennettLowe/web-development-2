178499237ce49e61113b6d73c8feb33f40c77a7a
mbennettlowe <mbennettlowe@gmail.com>
2018-08-03 14:56:44 -0400

    Investment Site with Bootstrap

---
+++ Sou Sou App/main.js
@@ -0,0 +1,53 @@
+//setup variable as an array to capture mem name//
+
+const members = [];
+
+//setup data for Object - Will this pull from Account Sign Up?
+const memberInfo = {
+name: "",
+location: = "",
+join date: "",
+}
+
+let mem1 = "";
+let mem2 = "";
+
+function memberInfo() {
+	console.log("Hello" + name);
+	// to loop through and Object
+	//for (let key in memberInfo) {
+		//console.log('key', key);
+		//console.log('value:', memberInfo[key]);
+	}
+}
+
+//Members joined and name added to Array automatically//
+//Lambda course 6 mentioned Objects much better structure than Array//
+
+function addMem(members) {
+	addMem.push[];
+	//for each to loop through an Array
+	for (let i=0; i < addMem.length; i++){
+		console.log(addMem[i]);
+	}
+
+	addMem.push(members);
+	console.log(members);
+
+}
+
+
+
+
+function addWeekly() {
+	let mem1 = value + 20;
+	return mem1;
+}
+
+addWeekly(20);
+
+
+
+function Contribution() {
+	console.log("I contributed");
+}
